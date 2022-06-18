844d81478ac1abb1644968d6f53c48bde5b9044f
sharqawidev <sharqawidev@gmail.com>
2022-06-18 13:33:48 +0300

    add sqlite to the project

--- server/datastore/index.ts
+++ server/datastore/index.ts
@@ -2,7 +2,13 @@ import { UserDao } from "./DAO/UserDao";
 import { PostDao } from './DAO/PostDao'
 import { LikeDao } from './DAO/LikeDao'
 import { CommentDao } from './DAO/CommentDao'
-import { InMemoryDataStore } from "./memoryDB";
+import { SqlDataStore } from "./sql";
+// import { InMemoryDataStore } from "./memoryDB";
 export interface DataStore extends UserDao, PostDao, LikeDao, CommentDao { }
 
-export const db = new InMemoryDataStore();
+export let db: DataStore;
+
+export async function initDB() {
+    // db = new InMemoryDataStore();
+    db = await new SqlDataStore().openDB();
+}
--- server/index.ts
+++ server/index.ts
@@ -1,37 +1,42 @@
 import express, { ErrorRequestHandler, RequestHandler } from "express"
 import { createPostHandler, listPostsHandler } from "./handlers/PostHandler";
 import asyncHandler from "express-async-handler"
-const app = express();
-// to handle json requests in express (express does not support json automatically)
-app.use(express.json())
+import { initDB } from "./datastore";
+
+(async () => {
+    await initDB();
+    const app = express();
+    // to handle json requests in express (express does not support json automatically)
+    app.use(express.json())
+
+    const requestsLoggerMiddleware: RequestHandler = (req, res, next) => {
+        /*
+         * 
+         * @param next to call the next middleware  
+         * 
+         * middleware is a point that the request will always passthrough before or after going to the request handler methods below (get and post), where you can do any logic you want, like authentication, logging, and printing any useful data.
+         */
+        console.log(`${req.method} request: ${req.path}\nBody: ${JSON.stringify(req.body)}\n`)
+        next();
+    }
+
+    app.use(requestsLoggerMiddleware)
+
+    app.get('/posts', asyncHandler(listPostsHandler));
+
+    app.post('/posts', asyncHandler(createPostHandler))
 
 
+    // error handler middleware so errors in the backend do not show to the client users
+    const errHandler: ErrorRequestHandler = (err, req, res, next) => {
+        console.log('Uncaught exception:', err);
+        return res.status(500).send('Oops! An error occurred, please try again later. ');
+    }
 
-const requestsLoggerMiddleware: RequestHandler = (req, res, next) => {
-    /*
-     * 
-     * @param next to call the next middleware  
-     * 
-     * middleware is a point that the request will always passthrough before or after going to the request handler methods below (get and post), where you can do any logic you want, like authentication, logging, and printing any useful data.
-     */
-    console.log(`${req.method} request: ${req.path}\nBody: ${JSON.stringify(req.body)}\n`)
-    next();
-}
+    app.use(errHandler)
 
-app.use(requestsLoggerMiddleware)
-
-app.get('/posts', asyncHandler(listPostsHandler));
-
-app.post('/posts', asyncHandler(createPostHandler))
+    app.listen(3000)
+})()
 
 
-// error handler middleware so errors in the backend do not show to the client users
-const errHandler: ErrorRequestHandler = (err, req, res, next) => {
-    console.log('Uncaught exception:', err);
-    return res.status(500).send('Oops! An error occurred, please try again later. ');
-}
-
-app.use(errHandler)
-
-app.listen(3000)
 
